6169de44082b37ba9c666ec1f3d5f9147d5e7c82
Rilyz71 <136700110+Rilyz71@users.noreply.github.com>
2024-04-27 19:05:12 +0700

    func: move_cursor_to_specified_window

--- main.py
+++ main.py
@@ -28,5 +28,27 @@ def get_all_windows():
     return all_windows
 
 
+def move_cursor_to_specified_window(title):
+    try:
+        # Ищем окно по его заголовку
+        window = gw.getWindowsWithTitle(title)
+        if window:
+            window = window[0]  # Берем первое окно, если найдено несколько
+            if window.isMinimized:
+                window.restore()  # Восстанавливаем окно, если оно свернуто
+
+            # Получаем координаты окна
+            center_x = window.left  # + (window.width // 2)
+            center_y = window.top  # + (window.height // 2)
+
+            # Перемещаем курсор к центру найденного окна
+            pyautogui.moveTo(center_x, center_y)
+            print(f"Курсор успешно перемещен в центр окна '{title}' на координаты ({center_x}, {center_y}).")
+        else:
+            print(f"Окно с заголовком '{title}' не найдено.")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
 if __name__ == "__main__":
-    pixel_match()
+    move_cursor_to_specified_window("(1) ChatGPT4 | Midjourney – (1)")
