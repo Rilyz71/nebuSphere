6c8a68fa7fb02b7f7e6a39363e9d65dca1470c7e
Rilyz71 <136700110+Rilyz71@users.noreply.github.com>
2024-04-27 16:32:31 +0700

    experemental

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import pyautogui
-import x_y_now_funcs
+import time
+
+X, Y = 2127, 371
 
 
 def pixel_match():
-    expected_color = (150, 110, 103)
+    expected_color = (60, 45, 48)
 
-    x, y = 2128, 366
+    x, y = 2127, 374
 
     if pyautogui.pixelMatchesColor(x, y, expected_color):
         print("Цвет пикселя совпадает с ожидаемым!!")
+        pyautogui.moveTo(x, y)
+        pyautogui.click(x, y)
+        expected_color = (242, 180, 162)
+        time.sleep(0.3)
+        if pyautogui.pixelMatchesColor(1940, 446, expected_color):
+            pyautogui.click(1940, 446)
+        else:
+            print("no")
     else:
         print("Цвет пикселя не совпадает с ожидаемым.(")
 
