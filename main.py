ec27bb24e2b58cd341001fc62a7775ae9ca1fd17
Rilyz71 <136700110+Rilyz71@users.noreply.github.com>
2024-04-27 15:52:09 +0700

    some changes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import pyautogui
+import x_y_now_funcs
 
-def main():
-    print("Hello first commit")
+
+def pixel_match():
+    expected_color = (150, 110, 103)
+
+    x, y = 2128, 366
+
+    if pyautogui.pixelMatchesColor(x, y, expected_color):
+        print("Цвет пикселя совпадает с ожидаемым!!")
+    else:
+        print("Цвет пикселя не совпадает с ожидаемым.(")
 
 
 if __name__ == "__main__":
-    main()
+    pixel_match()
--- x_y_now_funcs.py
+++ x_y_now_funcs.py
@@ -1,10 +1,16 @@
 import pyautogui
 
-current_position = pyautogui.position()
 
-x, y = current_position
-print(f'x = {x}, y = {y}')
+def get_current_color_and_position():
+    current_position = pyautogui.position()
+    x, y = current_position
+    print(f'x = {x}, y = {y}')
+    # функция pixel возвращает по x и y код цвета
+    current_color = pyautogui.pixel(x, y)
+    print(current_color)
 
-# функция pixel возвращает по x и y код цвета
-current_color = pyautogui.pixel(x, y)
-print(current_color)
+    return x, y, current_color
+
+
+if __name__ == "__main__":
+    get_current_color_and_position()
